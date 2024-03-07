45e433645769ab0b52502c12fa413661789eb883
JustAChillLemon <munimali250@gmail.com>
2024-03-06 23:25:25 -0500

    INITIAL COMMIT, trying to salvage code from Renee's World

---
+++ src/main/GamePanel.java
@@ -0,0 +1,96 @@
+package main;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GamePanel extends JPanel implements Runnable {
+    public static int tile_size = 48; // default tile size is 48
+    private int scale; // plans to use this to allow for window resizing
+
+    public static final int MAX_SCREEN_COL = 16; // used for how to draw tiles
+    public static final int MAX_SCREEN_ROW = 12;
+
+    // GAME LOOP
+    private Thread gameThread;
+    // TRACKING INPUT
+    KeyHandler keyH;
+    // TEST VARIABLES
+    int x = 0;
+    int y = 0;
+    public GamePanel() {
+        // setting up size of the panel
+        this.setPreferredSize(new Dimension(tile_size * MAX_SCREEN_COL, tile_size * MAX_SCREEN_ROW));
+        this.setBackground(Color.BLACK);
+        this.setDoubleBuffered(true);
+
+        keyH = new KeyHandler();
+        this.addKeyListener(keyH);
+        this.setFocusable(true);
+
+
+        startGameThread();
+        setUpWindow();
+
+    }
+
+    @Override
+    public void run() {
+        // variables needed to ensure its locked at 60 fps and below
+        long currentTime = System.nanoTime();
+        long previousTime = currentTime;
+        double delta = 0.0;
+        int FPS = 60;
+        double drawInterval = 1000000000.0 / FPS;
+
+        while (gameThread != null) {
+            // system.nanotime is java's very accurate clock or something (i dont 100% remember)
+            currentTime = System.nanoTime();
+
+            // the time between now and the least time this looped
+            delta += (double) (currentTime - previousTime) / drawInterval;
+
+            previousTime = currentTime;
+            if (delta >= 0.5) {
+                // delta being 1 or greater means 1/60 of a second;
+                repaint();
+                if (keyH.isUpKeyPressed()) {
+                    y--;
+                }
+                if (keyH.isDownKeyPressed()) {
+                    y++;
+                }
+                if (keyH.isRightKeyPressed()) {
+                    x++;
+                }
+                if (keyH.isLeftKeyPressed()) {
+                    x--;
+                }
+                delta = 0;
+            }
+        }
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        Graphics2D g2D = (Graphics2D) g;
+        g2D.setColor(Color.PINK);
+        g2D.fillRect(x, y, 100, 100);
+    }
+
+    private void setUpWindow() {
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("2D GAME");
+        window.add(this);
+        window.pack();
+        window.setVisible(true);
+    }
+
+    private void startGameThread() {
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+}
---
+++ src/main/KeyHandler.java
@@ -0,0 +1,83 @@
+package main;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyHandler implements KeyListener {
+
+    private boolean wKeyPressed, sKeyPressed, aKeyPressed, dKeyPressed; // movement keys
+    private boolean fKeyPressed; // interact key
+    private boolean upKeyPressed, downKeyPressed, leftKeyPressed, rightKeyPressed; // attack keys
+    public KeyHandler(){}
+    public boolean isUpKeyPressed() {
+        return upKeyPressed;
+    }
+
+    public boolean isDownKeyPressed() {
+        return downKeyPressed;
+    }
+
+    public boolean isLeftKeyPressed() {
+        return leftKeyPressed;
+    }
+
+    public boolean isRightKeyPressed() {
+        return rightKeyPressed;
+    }
+
+    public boolean isWKeyPressed() {
+        return wKeyPressed;
+    }
+    public boolean isAKeyPressed() {
+        return aKeyPressed;
+    }
+
+    public boolean isDKeyPressed() {
+        return dKeyPressed;
+    }
+
+    public boolean isSKeyPressed() {
+        return sKeyPressed;
+    }
+
+    public boolean isFKeyPressed() {
+        return fKeyPressed;
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int code = e.getKeyCode();
+
+        switch (code) {
+            case KeyEvent.VK_W -> wKeyPressed = true;
+            case KeyEvent.VK_S -> sKeyPressed = true;
+            case KeyEvent.VK_A -> aKeyPressed = true;
+            case KeyEvent.VK_D -> dKeyPressed = true;
+            case KeyEvent.VK_F -> fKeyPressed = true;
+            case KeyEvent.VK_UP -> upKeyPressed = true;
+            case KeyEvent.VK_DOWN -> downKeyPressed = true;
+            case KeyEvent.VK_LEFT -> leftKeyPressed = true;
+            case KeyEvent.VK_RIGHT -> rightKeyPressed = true;
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        switch (e.getKeyCode()) {
+            case KeyEvent.VK_W -> wKeyPressed = false;
+            case KeyEvent.VK_S -> sKeyPressed = false;
+            case KeyEvent.VK_A -> aKeyPressed = false;
+            case KeyEvent.VK_D -> dKeyPressed = false;
+            case KeyEvent.VK_UP -> upKeyPressed = false;
+            case KeyEvent.VK_DOWN -> downKeyPressed = false;
+            case KeyEvent.VK_LEFT -> leftKeyPressed = false;
+            case KeyEvent.VK_RIGHT -> rightKeyPressed = false;
+            case KeyEvent.VK_F -> fKeyPressed = false;
+        }
+    }
+}
