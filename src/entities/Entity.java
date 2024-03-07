9deb20964be3627bc69810717c2a1f75ca680f9f
JustAChillLemon <munimali250@gmail.com>
2024-03-07 14:02:36 -0500

    working on entity class

--- src/entities/Entity.java
+++ src/entities/Entity.java
@@ -1,13 +1,17 @@
 package entities;
 
-import java.awt.image.BufferedImage;
+import java.awt.*;
 
-public class Entity {
-    protected int worldX;
-    protected int worldY;
-
-    public Entity(int worldX, int worldY) {
+public abstract class Entity {
+    private int worldX;
+    private int worldY;
+    private String name;
+    private Rectangle hitbox;
+    public static final int TYPE_PLAYER = 0, TYPE_MOB = 1, TYPE_NPC = 2, TYPE_INTERACTABLE = 4;
+    public Entity(int worldX, int worldY, String name, int rectangleX, int rectangleY) {
         this.worldX = worldX;
         this.worldY = worldY;
+        this.name = name;
+        hitbox = new Rectangle(0,0, rectangleX, rectangleY);
     }
 }
