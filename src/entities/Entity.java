a73863ae6f4e0da54f1aba6d45c2be4415a61663
JustAChillLemon <munimali250@gmail.com>
2024-03-06 23:40:18 -0500

    working on entity superclass (beginning of my class design arc)

---
+++ src/entities/Entity.java
@@ -0,0 +1,13 @@
+package entities;
+
+import java.awt.image.BufferedImage;
+
+public class Entity {
+    protected int worldX;
+    protected int worldY;
+
+    public Entity(int worldX, int worldY) {
+        this.worldX = worldX;
+        this.worldY = worldY;
+    }
+}
