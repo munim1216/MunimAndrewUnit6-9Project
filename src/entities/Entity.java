f31c8c19d7bc2fdf25f3d87d281992c37c715e0f
BT_2N2_01 <example@bths.edu>
2024-03-11 14:04:40 -0400

    Game Character Class created

--- src/entities/Entity.java
+++ src/entities/Entity.java
@@ -3,15 +3,17 @@ package entities;
 import java.awt.*;
 
 public abstract class Entity {
-    private int worldX;
-    private int worldY;
-    private String name;
-    private Rectangle hitbox;
+    private int worldX; // x position in the whole map
+    private int worldY; // y position in the whole map
+    private String name; // name of the entity
+    private Rectangle hitbox; // hitbox of entity
+    private final int TYPE;
     public static final int TYPE_PLAYER = 0, TYPE_MOB = 1, TYPE_NPC = 2, TYPE_INTERACTABLE = 4;
-    public Entity(int worldX, int worldY, String name, int rectangleX, int rectangleY) {
+    public Entity(int worldX, int worldY, String name, int rectangleX, int rectangleY, int type) {
         this.worldX = worldX;
         this.worldY = worldY;
         this.name = name;
+        this.TYPE = type;
         hitbox = new Rectangle(0,0, rectangleX, rectangleY);
     }
 }
---
+++ src/entities/GameCharacter.java
@@ -0,0 +1,46 @@
+package entities;
+
+import java.awt.image.BufferedImage;
+
+public class GameCharacter extends Entity {
+    private BufferedImage[][] animations;
+    private int currentSprite; // the column in the 2d array of animations
+    private int typeOfSprite; // the row in the 2d array of animations, each row a different type of sprite
+    private int speed;
+    private int actionLockCounter;
+    private int health;
+    private int damage;
+    private int iframes;
+    private boolean isInvincible;
+    private boolean hasDied;
+    private int deathAnimationCounter;
+    private boolean isDeathAnimationOver;
+    private int direction;
+    public static final int DIRECTION_UP = 1, DIRECTION_DOWN = 2, DIRECTION_LEFT = 3, DIRECTION_RIGHT = 4;
+    public GameCharacter(int worldX,
+                         int worldY,
+                         String name,
+                         int rectangleX,
+                         int rectangleY,
+                         int type,
+                         int speed,
+                         BufferedImage[][] animations,
+                         int health,
+                         int damage
+    ) {
+        super(worldX, worldY, name, rectangleX, rectangleY, type);
+        this.speed = speed;
+        this.animations = animations;
+        this.health = health;
+        this.damage = damage;
+        direction = DIRECTION_DOWN;
+        currentSprite = 0;
+        typeOfSprite = 0;
+        actionLockCounter = 0;
+        iframes = 0;
+        isInvincible = false;
+        hasDied = false;
+        deathAnimationCounter = 0;
+        isDeathAnimationOver = false;
+    }
+}
