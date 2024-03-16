7b01ee648ec58fb86639ff342858311630ff9cc2
andre <73719598+andre-471@users.noreply.github.com>
2024-03-16 15:23:10 -0400

    EntityManager.collidesWith compelte

--- src/entities/EntityManager.java
+++ src/entities/EntityManager.java
@@ -1,10 +1,10 @@
 package entities;
 
-import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 
 public class EntityManager {
     private ArrayList<Entity> entities;
+
     public EntityManager() {
         entities = new ArrayList<>();
     }
@@ -17,21 +17,10 @@ public class EntityManager {
         ArrayList<Entity> collidesWith = new ArrayList<>();
         for (Entity otherEntity : entities) {
             if (entity.collidesWith(otherEntity)) {
-                collidesWith.add(entity);
+                collidesWith.add(otherEntity);
             }
         }
 
         return collidesWith;
-    }
-
-    public static void main(String[] args) {
-        EntityManager entityManager = new EntityManager();
-        GameCharacter gameCharacter = new GameCharacter(1,1,"1",1,1,EntityType.MOB, new BufferedImage[][]{}, 1, 1, 1);
-        GameCharacter gameCharacter1 = new GameCharacter(1,1,"1",1,1,EntityType.MOB, new BufferedImage[][]{}, 1, 1, 1);
-        GameCharacter gameCharacter2 = new GameCharacter(1,1,"1",1,1,EntityType.MOB, new BufferedImage[][]{}, 1, 1, 1);
-        GameCharacter gameCharacter3 = new GameCharacter(1,1,"1",1,1,EntityType.MOB, new BufferedImage[][]{}, 1, 1, 1);
-
-        entityManager.addEntity(gameCharacter);
-        entityManager.addEntity(gameCharacter1);
     }
 }
