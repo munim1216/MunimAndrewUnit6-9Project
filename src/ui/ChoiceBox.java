4c8973f23ecda8110af2012ae7340d512cabf3f5
BT_1N3_06 <ropqwfn@afwupffifasdcbqefcbdvwergwrvivervbfverijhbvvievadybvauiaiuaduvadfygavdvwverilvbwevilwebvklajfilsdbfasdibcalibzuycblefosdASLalyu;ffjasifwduwdisdakflufsaifasdaf;ilakaouifv;aeaofsfvjjasdbfyhbefsduilsdiugasdfilfiuasweflbfweilweflasklbfahalfabwelzvijfncak;fausd;kgaklcbweflawek.jf;ofofilvbdviweweipknqfiudfd.zil;Nnvo;ieeeweph[q0ijfvk;jzsia.mfbdf9ohao;nluiweajntriangle>
2024-03-25 11:03:35 -0400

    wroking on fixing

--- src/ui/ChoiceBox.java
+++ src/ui/ChoiceBox.java
@@ -3,9 +3,20 @@ package ui;
 import java.awt.*;
 
 public class ChoiceBox extends BaseUI {
-    public ChoiceBox() {}
+    private int x;
+    private int y;
+    private int width;
+    private int height;
+    private Color backgroundColor;
+    private Color outlineColor;
+    private String message;
+    private String choice1;
+    private String choice2;
+    public ChoiceBox(int x, int y, int width, int height, Color backgroundColor, Color outlineColor, String message, String choice1, String choice2) {
 
-    public void drawChoiceBox(Graphics2D g2D, int x, int y, int width, int height, Color backgroundColor, Color outlineColor, String message, String choice1, String choice2) {
+    }
+
+    public void drawChoiceBox(Graphics2D g2D, ) {
         drawBoxWithMessage(g2D, x, y, width, height, backgroundColor, outlineColor, message);
         g2D.drawString(choice1, width, height);
         Rectangle a = (Rectangle) g2D.getFontMetrics().getStringBounds(choice1, g2D);
